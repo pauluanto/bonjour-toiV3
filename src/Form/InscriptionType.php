fff1f2b793e4f87ac646abdcf41de2175aac525f
pa.morelli <pa.morelli@qwant.com>
2020-01-04 12:28:49 +0100

    add email but is not unique

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +15,7 @@ class InscriptionType extends AbstractType
     {
         $builder
             ->add('username')
+            ->add('email', EmailType::class)
             ->add('password',PasswordType::class)
             ->add('verifPassword',PasswordType::class)
         ;
